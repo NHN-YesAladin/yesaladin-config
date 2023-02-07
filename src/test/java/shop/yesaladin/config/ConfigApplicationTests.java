317ca7ddefe51e1862bf70ff46728f70b630f622
Hongdae Kim <mongmeo.dev@gmail.com>
2023-02-07 10:52:37 +0900

    first commit

---
+++ src/test/java/shop/yesaladin/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.yesaladin.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
